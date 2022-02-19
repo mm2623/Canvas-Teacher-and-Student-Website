5d8293959b9ca896dab9add73a3747a495b45802
Muhammad Abdul Mannan <90267420+mm2623@users.noreply.github.com>
2022-02-19 00:21:37 -0500

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -7,7 +7,6 @@
     <div class="tab">
         <h1>Sidd & Associates, LLC</h1>
         <p>Sidd & Associates was established by the founder and CEO of Meco Electric. Jay Siddiqui created Sidd & Associates as a daughter company of Meco, striving to serve the state of New Jersey, his home. As Meco has taken New York by storm, Sidd is dedicated to providing the same expertise and diligence as our parent company has been demonstrating for over twenty-five years.</p>
-        <p>Visit us on Facebook : <a href="https://www.facebook.com/SiddAssociatesLLC">https://www.facebook.com/SiddAssociatesLLC</a></p>
     </div>
 </body>
 <?php require_once "footer.php"; ?>
