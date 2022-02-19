2c4815b55d251a2e55b520bdb292d2fd746fc6df
Muhammad Abdul Mannan <90267420+mm2623@users.noreply.github.com>
2022-02-19 00:32:32 -0500

    Add files via upload

--- public_html/current-jobs.php
+++ public_html/current-jobs.php
@@ -1,7 +1,7 @@
 <?php require_once "nav.php"; ?>
     <div class="container-current-jobs">
 		<h1 class="header-current-jobs">Current Jobs</h1>
-        <p>Sidd & Associates, LLC is currently working on the following jobs:
+        <p>Sidd & Associates, LLC is currently working on the following jobs:</p>
         <p>1. Boardwalk reconstruction for the Borough of Seaside Heights</p>
         <p>2. North River Water Pollution Control Plant</p>
         <p>3. Wards Island DEP</p>
