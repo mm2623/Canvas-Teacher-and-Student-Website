ba6510010af1c710ddbdd2fe1624f87cd16e035f
Muhammad Abdul Mannan <90267420+mm2623@users.noreply.github.com>
2022-02-21 21:57:56 -0500

    Add files via upload

--- public_html/contact.php
+++ public_html/contact.php
@@ -4,16 +4,16 @@
     <p><u>Sidd Plaza, 500 Rt 33 W Suite LLA, Millstone, New Jersey 08535, United States</u></p>
     <form method="#" >
 
-      <label for="name">*Name</label>
+      <label for="name"><spam style="color: #FF0000;">*</spam>Name</label>
       <input type="text" id="name" name="name" placeholder="Your name...">
 
       <label for="name">Phone</label>
       <input type="text" id="phone" name="Phone" placeholder="Your phone...">
 
-      <label for="email">*Email</label>
+      <label for="email"><spam style="color: #FF0000;">*</spam>Email</label>
       <input type="text" id="email" name="email" placeholder="Your Email Address...">
 
-      <label for="message">*Message</label>
+      <label for="message"><spam style="color: #FF0000;">*</spam>Message</label>
       <textarea id="subject" name="message" placeholder="Your Message..." style="height:144px"></textarea>
 
       <input type="submit" value="Submit">
